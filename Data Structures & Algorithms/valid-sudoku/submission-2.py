286d0d029499b3e936db0313a1c9110fcaf91094
Hamin Han <33069589+Hamin0702@users.noreply.github.com>
2026-05-21 23:31:02 -0400

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,53 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        validRow = defaultdict(set)
+        validCol = defaultdict(set)
+        validSquares = defaultdict(set) # key: (r//3, c//3)
+
+        for row in range(9):
+            for col in range(9):
+
+                val = board[row][col]
+
+                if val == ".":
+                    continue
+                elif val in validRow[row] or val in validCol[col] or val in validSquares[(row//3, col//3)]:
+                    return False
+                else:
+                    validRow[row].add(val)
+                    validCol[col].add(val)
+                    validSquares[(row//3, col//3)].add(val)
+        
+        return True
+        # for row in range(9):
+        #     validRows = set()
+        #     for col in range(9):
+        #         val = board[row][col]
+        #         if val != "." and val not in validRows:
+        #             validRows.add(val)
+        #         elif val in validRows:
+        #             return False
+
+        # for col in range(9):
+        #     validCols = set()
+        #     for row in range(9):
+        #         val = board[row][col]
+        #         if val != "." and val not in validCols:
+        #             validCols.add(val)
+        #         elif val in validCols:
+        #             return False
+
+        # for square in range(9):
+        #     validSquares = set()
+        #     for i in range(3):
+        #         for j in range(3):
+        #             row = (square//3) * 3 + i
+        #             col = (square % 3) * 3 + j
+        #             if board[row][col] == ".":
+        #                 continue
+        #             if board[row][col] in validSquares:
+        #                 return False
+        #             validSquares.add(board[row][col])
+
+        # return True
